0c3195baf8ed7a983f78a09254a2d0f4a0ff9be0
skalyanmoguloju <kalyansaim@gmail.com>
2016-02-18 18:49:30 -0600

    Main Setup

---
+++ src/main/java/com/fundamental/proj/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.fundamental.proj.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by sai on 2/17/16.
+ */
+@Controller
+public class HomeController {
+    @RequestMapping
+    public String home(){
+        return "login";
+    }
+
+    @RequestMapping(value = "/forgot_password")
+    public String he()
+    {
+        return "forgot";
+    }
+
+    @RequestMapping(value = "/session")
+    @ResponseBody
+    public String Logined_in() {
+        //HYBERNET CALLS
+        return "Logged In";
+    }
+
+}
+
---
+++ src/main/java/com/fundamental/proj/util/AppConfig.java
@@ -0,0 +1,30 @@
+package com.fundamental.proj.util;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by sai on 2/17/16.
+ */
+@Configuration
+@EnableWebMvc
+public class AppConfig extends WebMvcConfigurerAdapter{
+    @Bean
+    public InternalResourceViewResolver viewResolver()
+    {
+        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
+        internalResourceViewResolver.setPrefix("/");
+        internalResourceViewResolver.setSuffix(".jsp");
+        return internalResourceViewResolver;
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
+
