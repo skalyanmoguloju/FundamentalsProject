6709d28abd23f212912fac074fdbb3306cadf9f8
madelineisabelle <Madeline@MacBook-Air-2.local>
2016-03-21 23:30:14 -0500

    done with bean tests

--- src/test/java/com/fundamental/proj/bean/UserBeanSteps.java
+++ src/test/java/com/fundamental/proj/bean/UserBeanSteps.java
@@ -8,7 +8,6 @@ import org.mockito.Mock;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
-import cucumber.api.PendingException;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import java.util.Date;
